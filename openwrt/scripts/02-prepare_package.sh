38217a0b6d9548992e6274fc85b5751c44946c80
0118Add <60157610+0118Add@users.noreply.github.com>
2024-07-18 13:07:52 +0800

    Update 02-prepare_package.sh

--- openwrt/scripts/02-prepare_package.sh
+++ openwrt/scripts/02-prepare_package.sh
@@ -57,8 +57,8 @@ git clone https://github.com/QiuSimons/luci-app-daed-next package/new/daed-next
 #git clone https://github.com/muink/luci-app-homeproxy package/new/homeproxy
 git clone https://github.com/lxiaya/openwrt-homeproxy package/new/homeproxy
 #git clone https://$github/immortalwrt/homeproxy package/new/homeproxy
-sed -i "s/ImmortalWrt/OpenWrt/g" package/new/homeproxy/po/zh_Hans/homeproxy.po
-sed -i "s/ImmortalWrt proxy/OpenWrt proxy/g" package/new/homeproxy/htdocs/luci-static/resources/view/homeproxy/{client.js,server.js}
+sed -i "s/ImmortalWrt/OpenWrt/g" package/new/homeproxy/luci-app-homeproxy/po/zh_Hans/homeproxy.po
+sed -i "s/ImmortalWrt proxy/OpenWrt proxy/g" package/new/homeproxy/luci-app-homeproxy/htdocs/luci-static/resources/view/homeproxy/{client.js,server.js}
 
 # alist
 #git clone https://$github/sbwml/openwrt-alist package/new/alist
