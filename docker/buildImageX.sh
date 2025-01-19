ed6475f03f3896ed8f707231cb73cb40fcb2157a
0118Add <60157610+0118Add@users.noreply.github.com>
2025-01-19 10:27:12 +0800

    Update buildImageX.sh

--- docker/buildImageX.sh
+++ docker/buildImageX.sh
@@ -23,7 +23,7 @@ rm -f "$TMPDIR/etc/bench.log" && \
 #sed -e "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/" -i "${TMPDIR}/etc/ssh/sshd_config" && \
 sss=$(date +%s) && \
 ddd=$((sss/86400)) && \
-sed -e "s/:0:0:99999:7:::/:${ddd}:0:99999:7:::/" -i "${TMPDIR}/etc/shadow" && \
+#sed -e "s/:0:0:99999:7:::/:${ddd}:0:99999:7:::/" -i "${TMPDIR}/etc/shadow" && \
 #echo "17 3 * * * /etc/coremark.sh" >> "$TMPDIR/etc/crontabs/root" && \
 rm -rf "$TMPDIR/lib/firmware/*" "$TMPDIR/lib/modules/*" && \
 (cd "$TMPDIR" && tar cf ../openwrt-armvirt-64-generic-rootfs-patched.tar .) && \
