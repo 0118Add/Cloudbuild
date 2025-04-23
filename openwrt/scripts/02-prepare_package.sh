78dff25049299f572b329fb1fc156a4cfa73f03d
0118Add <60157610+0118Add@users.noreply.github.com>
2025-04-23 13:19:35 +0800

    Update 02-prepare_package.sh

--- openwrt/scripts/02-prepare_package.sh
+++ openwrt/scripts/02-prepare_package.sh
@@ -2,7 +2,7 @@
 
 # golang 1.23
 rm -rf feeds/packages/lang/golang
-git clone https://$github/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+git clone https://$github/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
 # node - prebuilt
 rm -rf feeds/packages/lang/node
